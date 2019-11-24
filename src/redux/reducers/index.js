06ddcc600587960d7f1f2673cccfc3d10dd5c743
seeeyoul <1364661512@qq.com>
2019-11-24 11:25:02 +0800

    aa

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux-immutable";
 import userMana from "./userMana";
 import resource from "./resource";
+import announce from "./announce";
 
 export default combineReducers({
     userMana,
-    resource
+    resource,
+    announce,
 })
